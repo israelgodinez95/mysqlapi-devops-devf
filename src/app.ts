f4f516586f6867adef20662624190b32bdfa4335
Israel Godinez B <isra.gb47@gmail.com>
2024-02-14 23:50:20 -0600

    starting node-mysql-api app using TS

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import ip from 'ip';
+import patientRoutes from './routes/patient.routes';
+import { HttpResponse } from './domain/response';
+import { Code } from './enum/code.enum';
+import { Status } from './enum/status.enum';
+
+export class App {
+    private readonly app: Application;
+    private readonly APPLICATION_RUNNING = 'application is running on:';
+    private readonly ROUTE_NOT_FOUND = 'Route does not exist on the server';
+    constructor(private readonly port: (string | number) = process.env.SERVER_PORT || 3000) {
+        this.app = express();
+        this.middleWare();
+        this.routes();
+    }
+
+    listen(): void {
+        this.app.listen(this.port)
+        console.info(`${this.APPLICATION_RUNNING} ${ip.address()}:${this.port}`);
+    }
+
+   private middleWare() {
+        this.app.use(cors({ origin: '*' }));
+        this.app.use(express.json());
+    }
+    private routes(): void {
+        this.app.use('/patients', patientRoutes);
+        this.app.get('/',(req: Request, res: Response) => res.status(Code.OK).send(new HttpResponse(Code.OK, Status.OK, 'Welcome to the Patients API v1.0.0')));
+        this.app.all('*',(req: Request, res: Response) => res.status(Code.NOT_FOUND).send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, this.ROUTE_NOT_FOUND)));
+    }
+}
